7a80e4be5fa84f23f0a65c45c2e2c409789e50fa
CMHayden <haydencallum4@gmail.com>
2019-07-29 17:10:35 +0200

    adds posting from table to api/products which can then forward the data on to elsewhere on the interweb

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/handsontable@latest/dist/handsontable.full.min.css">
         <link rel="stylesheet" type="text/css" href="https://handsontable.com/static/css/main.css">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
         <style>
             h1, p {
@@ -15,7 +16,7 @@
             }
 
             .center-align-handsontable {
-                width: 900px;
+                width: 1100px;
                 margin: auto;
             }
         </style>
@@ -29,21 +30,13 @@
             <div id="hot"></div>
             <div id="export-buttons" class="visible">
                 <button id="export-csv" class="btn size-medium bg-blue text-white shadow hover-moveup" style="margin-right: 5px;">Export as .csv</button>
-                <button id="store-api" class="btn size-medium bg-green text-white shadow hover-moveup">Store in API</button>
+                <button id="store-api" class="btn size-medium bg-green text-white shadow hover-moveup">Send to API</button>
             </div>
         </div>
 
+
         <script src="https://cdn.jsdelivr.net/npm/handsontable@latest/dist/handsontable.full.min.js"></script>
         <script>
-
-            /*
-                Test Data:
-                487545	V-Neck T-Shirt	€4.50
-                487546	Chinos	€15
-                487547	Jeans	€12.50
-                487548	ACDC T-Shirt	€7.50
-                487549	Metallica T-Shirt	€7.50
-            */
 
             function table(){
                 var hotElement = document.querySelector('#hot');
@@ -51,85 +44,75 @@
                 var hotSettings = {
                     columns: [
                     {
-                        data: 'sku',
+                        data: 'ProductName',
+                        type: 'text',
+                        width: 60
+                    },
+                    {
+                        data: 'ProductModel',
+                        type: 'text',
+                        width: 60
+                    },
+                    {
+                        data: 'Description',
+                        type: 'text'
+                    },
+                    {
+                        data: 'SKU',
                         type: 'numeric',
                         width: 40
                     },
                     {
-                        data: 'description',
-                        type: 'text'
-                    },
-                    {
-                        data: 'price',
-                        type: 'text'
+                        data: 'Price',
+                        type: 'numeric',
+                        width: 40
                     }
 
                     ],
                     stretchH: 'all',
-                    width: 837,
+                    width: 1037,
                     autoWrapRow: true,
+                    manualRowResize: true,
+                    manualColumnResize: true,
                     height: 300,
-                    maxRows: 22,
                     rowHeaders: true,
                     colHeaders: [
-                    'SKU',
+                    'Name',
+                    'Model',
                     'Description',
+                    'SKU',
                     'Price'
                     ],
-                    exportFile: true
+                    dropdownMenu: true,
+                    exportFile: true,
+                    contextMenu: true
                 };
-                var hot = new Handsontable(hotElement, hotSettings);
-                document.getElementById("export-csv").addEventListener("click", function(event) { hot.getPlugin("exportFile").downloadFile("csv", {filename: "product.management.addProdcut"});})
-                document.getElementById("store-api").addEventListener('click', function() {
-                    var exportedString = hot.getPlugin("exportFile").exportAsString('csv', {
-                    bom: true,
-                    columnDelimiter: ',',
-                    columnHeaders: false,
-                    exportHiddenColumns: true,
-                    exportHiddenRows: true,
-                    mimeType: 'text/csv',
-                    rowDelimiter: '\r\n',
-                    rowHeaders: true
-                    });
+                    var hot = new Handsontable(hotElement, hotSettings);
+                    document.getElementById("export-csv").addEventListener("click", function(event) { hot.getPlugin("exportFile").downloadFile("csv", {filename: "product.management.addProdcut"});})
 
-                    let input = hot.getData();
-                    let i = 0;
-                    let j = 1;
+                    document.getElementById("store-api").addEventListener('click', function() {
 
-                    while (input[i] != null) {
 
-                        if (input[i][0] == null || input[i][1] == null || input[i][2] == null || input[i][0] == "" || input[i][1] == "" || input[i][2] == "" ){
-                            alert("Invalid entry in row " + j + ". All previous rows stored");
-                            return 0;
-                        }
+                    const headers = new Headers()
+                    headers.append("Content-Type", "application/json")
 
-                        const headers = new Headers()
-                        headers.append("Content-Type", "application/json")
+                    const body = hot.getData();
 
-                        const body = {
-                            "SKU": input[i][0],
-                            "Description": input[i][1],
-                            "Price": input[i][2]
-                        }
-
-                        const options = {
+                    const options = {
                         method: "POST",
                         headers,
                         mode: "cors",
                         body: JSON.stringify(body),
                         }
+                        fetch("/api/products", options)
 
-                        fetch("https://entpebyij95.x.pipedream.net/", options)
+                        console.log(hot.getData());
+                        console.log(JSON.stringify(hot.getData()));
 
-                        i++
-                        j++
-                    }
+                    });
 
-                    console.log("Success! This is the data sent to request bin: ");
-                    console.log(hot.getData());
+                };
 
-                });
-            }
         </script>
     </body>
 </html>
